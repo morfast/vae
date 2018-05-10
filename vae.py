1f1d3a05fefad2027eb8d2f9e1f5949489162b70
morfast <morfast@example.com>
2018-05-10 16:59:43 +0800

    add training

--- vae.py
+++ vae.py
@@ -8,21 +8,6 @@ import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data')
 
-tf.reset_default_graph()
-
-batch_size = 64
-
-X_in = tf.placeholder(dtype = tf.float32, shape=[None, 28, 28], name = "X")
-Y = tf.placeholder(dtype = tf.float32, shape=[None, 28, 28], name = "Y")
-Y_flat = tf.reshape(Y, shape = [-1, 28*28])
-keep_prob = tf.placeholder(dtype = tf.float32, shape = (), name='keep_prob')
-
-dec_in_channels = 1
-n_latent = 8
-
-reshaped_dim = [-1, 7, 7, dec_in_channels]
-inputs_decoder = 49 * dec_in_channels / 2
-
 def lrelu(x, alpha = 0.3):
     return tf.maximum(x, tf.multiply(x, alpha))
 
@@ -40,7 +25,8 @@ def encoder(X_in, keep_prob):
         x = tf.contrib.layers.flatten(x)
         # mean, len=n_latent
         mn = tf.layers.dense(x, units=n_latent)
-        # variance, len=n_latent
+        # the output of the NN is log(variance*2), len=n_latent
+        # sd = log(variance)
         sd = 0.5 * tf.layers.dense(x, units=n_latent)
         # reparameterization
         epsilon = tf.random_normal(tf.stack([tf.shape(x)[0], n_latent]))
@@ -67,27 +53,62 @@ def decoder(sampled_z, keep_prob):
 
         return img
 
+tf.reset_default_graph()
+
+batch_size = 64
+
+X_in = tf.placeholder(dtype = tf.float32, shape=[None, 28, 28], name = "X")
+Y = tf.placeholder(dtype = tf.float32, shape=[None, 28, 28], name = "Y")
+Y_flat = tf.reshape(Y, shape = [-1, 28*28])
+keep_prob = tf.placeholder(dtype = tf.float32, shape = (), name='keep_prob')
+
+dec_in_channels = 1
+n_latent = 8
+
+reshaped_dim = [-1, 7, 7, dec_in_channels]
+inputs_decoder = 49 * dec_in_channels / 2
+
 
 sampled, mn, sd = encoder(X_in, keep_prob)
 dec = decoder(sampled, keep_prob)
 
 unreshaped = tf.reshape(dec, [-1, 28*28])
+
+# loss function
 img_loss = tf.reduce_sum(tf.squared_difference(unreshaped, Y_flat), 1)
+# the KL divergence of the N(mean, variance^2) and N(0, 1)
+# = 1/2 * (-log(variance^2) + mean^2 + variance^2 -1)
 latent_loss = -0.5 * tf.reduce_sum(1.0 + 2.0 * sd - tf.square(mn) - tf.exp(2.0 * sd), 1)
 loss = tf.reduce_mean(img_loss + latent_loss)
+
 optimizer = tf.train.AdamOptimizer(0.0005).minimize(loss)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-for i in range(1000):
+for i in range(200):
+    # batch_size samples(pictures) in list(batch), each 28*28
     batch = [np.reshape(b, [28, 28]) for b in mnist.train.next_batch(batch_size=batch_size)[0]]
     sess.run(optimizer, feed_dict = {X_in:batch, Y:batch, keep_prob: 0.8})
+    print i
 
     if i % 200 == 0:
         ls, d, i_ls, d_ls, mu, sigm = sess.run([loss, dec, img_loss, latent_loss, mn, sd], feed_dict = {X_in: batch, Y: batch, keep_prob: 1.0})
+        # reconstructed picture
         plt.imshow(np.reshape(batch[0], [28, 28]), cmap='gray')
         plt.savefig(str(i) + ".png")
         plt.imshow(d[0], cmap='gray')
         plt.savefig(str(i) + ".png")
         print(i, ls, np.mean(i_ls), np.mean(d_ls))
 
+#def generate(sess, dec):
+print "training completed"
+randoms = [np.random.normal(0, 1, n_latent) for _ in range(10)]
+imgs = sess.run(dec, feed_dict = {sampled: randoms, keep_prob: 1.0})
+imgs = [np.reshape(imgs[i], [28, 28]) for i in range(len(imgs))]
+
+for i,img in enumerate(imgs):
+    print "generating pic", i
+    plt.figure(figsize=(1,1))
+    plt.axis('off')
+    plt.imshow(img, cmap='gray')
+    plt.savefig("g" + str(i) + ".png")
