ea4a3935a1e69a86b182d2467b9eec4f20bbd715
morfast <morfast@example.com>
2018-05-10 14:17:56 +0800

    rename

--- vae.py
+++ vae.py
@@ -31,16 +31,20 @@ def encoder(X_in, keep_prob):
     activation = lrelu
     with tf.variable_scope("encoder", reuse=None):
         X = tf.reshape(X_in, shape=[-1, 28, 28, 1])
-        #x = tf.layers.conv2d(X, filters=64, kernel_size=4, strides=2, padding='same', activation=activation)
-        #x = tf.nn.dropout(x, keep_prob)
+        x = tf.layers.conv2d(X, filters=64, kernel_size=4, strides=2, padding='same', activation=activation)
+        x = tf.nn.dropout(x, keep_prob)
         x = tf.layers.conv2d(X, filters=64, kernel_size=4, strides=2, padding='same', activation=activation)
         x = tf.nn.dropout(x, keep_prob)
         x = tf.layers.conv2d(x, filters=64, kernel_size=4, strides=1, padding='same', activation=activation)
         x = tf.nn.dropout(x, keep_prob)
         x = tf.contrib.layers.flatten(x)
+        # mean, len=n_latent
         mn = tf.layers.dense(x, units=n_latent)
+        # variance, len=n_latent
         sd = 0.5 * tf.layers.dense(x, units=n_latent)
+        # reparameterization
         epsilon = tf.random_normal(tf.stack([tf.shape(x)[0], n_latent]))
+        # latent vector
         z = mn + tf.multiply(epsilon, tf.exp(sd))
 
     return z, mn, sd
@@ -51,8 +55,8 @@ def decoder(sampled_z, keep_prob):
         x = tf.layers.dense(sampled_z, units=inputs_decoder, activation=activation)
         x = tf.layers.dense(x, units=inputs_decoder * 2 + 1, activation= activation)
         x = tf.reshape(x, reshaped_dim)
-        #x = tf.layers.conv2d_transpose(x, filters=64, kernel_size=4, strides=2, padding='same', activation=tf.nn.relu)
-        #x = tf.nn.dropout(x, keep_prob)
+        x = tf.layers.conv2d_transpose(x, filters=64, kernel_size=4, strides=2, padding='same', activation=tf.nn.relu)
+        x = tf.nn.dropout(x, keep_prob)
         x = tf.layers.conv2d_transpose(x, filters=64, kernel_size=4, strides=2, padding='same',activation=tf.nn.relu)
         x = tf.nn.dropout(x, keep_prob)
         x = tf.layers.conv2d_transpose(x, filters=64, kernel_size=4, strides=1, padding='same',activation=tf.nn.relu)
